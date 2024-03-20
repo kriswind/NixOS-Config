8cd6f14e474ddb969997c238f8b9c342e56fe4e7
kriswind <126849955+kriswind@users.noreply.github.com>
2024-03-20 12:11:30 +0200

    12:11

--- configuration.nix
+++ configuration.nix
@@ -116,14 +116,15 @@
     };
   };
 
+  nixpkgs.config = {
+    # Allow proprietary packages
+    allowUnfree = true;
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # Create an alias for the unstable channel
-  packageOverrides = pkgs: {
-    unstable = import <nixos-unstable> { # pass the nixpkgs config to the unstable alias # to ensure `allowUnfree = true;` is propagated:
-      config = config.nixpkgs.config;
+    # Create an alias for the unstable channel
+    packageOverrides = pkgs: {
+      unstable = import <nixos-unstable> { # pass the nixpkgs config to the unstable alias # to ensure `allowUnfree = true;` is propagated:
+        config = config.nixpkgs.config;
+      };
     };
   };
 
