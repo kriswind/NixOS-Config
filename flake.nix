9715962241479d9d753e7cce43176024688831e3
kriswind <126849955+kriswind@users.noreply.github.com>
2024-03-20 09:50:16 +0200

    meow

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    
+    # this line assume that you also have nixpkgs as an input
+    nix-ld = {
+      url = "github:Mic92/nix-ld";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nix-ld, self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./configuration.nix
+        inputs.home-manager.nixosModules.default
+
+        # ... add this line to the rest of your configuration modules
+        nix-ld.nixosModules.nix-ld
+
+        # The module in this repository defines a new module under (programs.nix-ld.dev) instead of (programs.nix-ld) 
+        # to not collide with the nixpkgs version.
+        { programs.nix-ld.dev.enable = true; }
+      ];
+    };
+  };
+}
